06466857253ef135d337cb430932be6c0131baf4
Iumaloo <67984789+Iumaloo@users.noreply.github.com>
2020-07-10 22:21:17 -0500

    Add files via upload

---
+++ Binario_a_decimal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BinarioAdecimal
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Ingrese números binarios");
+            Console.WriteLine("Ingrese b4");
+
+            int b4 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese b3");
+            int b3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese b2");
+            int b2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese b1");
+            int b1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese b0");
+            int b0 = int.Parse(Console.ReadLine());
+
+            double dec1 = (0 * 2 + b4);
+            double dec2 = (dec1 * 2 + b3);
+            double dec3 = (dec2 * 2 + b2);
+            double dec4 = (dec3 * 2 + b1);
+
+            double Numero_Final = (dec4*2 +b0);
+
+            Console.WriteLine("Decimal= " + Numero_Final);
+        }
+    }
+}
