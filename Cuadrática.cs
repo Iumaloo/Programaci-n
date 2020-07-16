23400fe5186fb12e7b09dd3fa8a71d60173bbc86
Iumaloo <67984789+Iumaloo@users.noreply.github.com>
2020-07-16 10:48:28 -0500

    Add files via upload

---
+++ Cuadrática.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Fórmula_cuadrática
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Ingrese a");
+
+            double a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese b");
+
+            double b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese c");
+
+            double c = double.Parse(Console.ReadLine());
+
+            double disc= b * b - 4 * a * c;
+
+            if (disc==0)
+                    {
+               
+                double resultado1 = (-b  / (2 * a));
+                Console.WriteLine("Solución única : " + resultado1);
+            }
+            
+            else if (disc >0)
+            {
+
+                double resultado = (-b + (Math.Sqrt(disc))) / (2 * a);
+                double resultado2 = (-b - (Math.Sqrt(disc)))/ (2 * a);
+                Console.WriteLine("Tiene dos soluciones : " + resultado);
+                    Console.WriteLine( resultado2 );
+            }
+
+            else
+            {
+                Console.WriteLine("No hay solución : " + disc);
+            }
+
+            
+        }
+    }
+}
---
+++ Indice.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace IMC
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("Ingrese su estatura en centímetros");
+            double estatura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese su peso en kilogramos");
+            double peso = double.Parse(Console.ReadLine());
+
+            double estaturac = (estatura / 100);
+            double IMC = (peso / (estaturac * estaturac));
+            
+
+            if (IMC < 18.5)
+            {
+                Console.WriteLine("Su índice es : " + IMC);
+                Console.WriteLine("Usted está bajo de peso");
+            }
+
+            else if (18.5 <= IMC && IMC < 25)
+            {
+                Console.WriteLine("Su índice es : " + IMC);
+                Console.WriteLine("Usted tiene peso normal");
+            }
+
+            else if (25.0 <= IMC && IMC < 30)
+            {
+                Console.WriteLine("Su índice es : " + IMC);
+                Console.WriteLine("Usted tiene sobrepeso");
+            }
+
+            else 
+
+            {
+                Console.WriteLine("Su índice es : " + IMC);
+                Console.WriteLine("Usted tiene obesidad");
+            }
+        }
+    }
+}
