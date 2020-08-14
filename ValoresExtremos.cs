797b4dbc4e8823c0ff8f4fc5a5101bf9bd44c8ea
Iumaloo <67984789+Iumaloo@users.noreply.github.com>
2020-08-14 09:20:08 -0500

    Add files via upload

---
+++ ValoresExtremos.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+using System.Xml.Schema;
+
+namespace Valores_extremos
+{
+    class Program
+    {
+        static void Main()
+        {
+            double total = 0;
+            Console.WriteLine("Ingrese el número de datos");
+            int n = int.Parse(Console.ReadLine());
+            int i = 0, max = 0 , min = 500;
+            string nombremax = "";
+            string nombremin = "";
+
+
+            while (i < n)
+            {
+
+                Console.WriteLine("Ingrese nombre, luego la edad");
+                string nombre = Console.ReadLine();
+                int edad = int.Parse(Console.ReadLine());
+
+                if (edad > max)
+                {
+                    max = edad;
+                    nombremax = nombre; 
+
+                }
+                if (edad < min)
+                {
+                    min = edad;
+                    nombremin = nombre; 
+                }
+
+                total += edad;
+
+                i++;
+            }
+
+            double promedio = total / n;
+            Console.WriteLine("Promedio : " + promedio);
+            Console.WriteLine("Mayor : " + max + "," + nombremax);
+            Console.WriteLine("Menor : " + min+ ","+  nombremin);
+          
+        }
+
+        
+
+    }
+}
