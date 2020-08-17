2d3f0ab345e572c3d5f8dcc70bbe493bd09851fd
Iumaloo <67984789+Iumaloo@users.noreply.github.com>
2020-08-17 17:58:11 -0500

    Add files via upload

---
+++ Euler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace Euler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el valor de X");
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el número de iteraciones");
+            int n = int.Parse(Console.ReadLine());
+            double suma = 0;
+       
+            for (int i= 0;i<n;i++)
+            {
+                suma+=(Math.Pow(x, i)/Factorial(i));
+            }
+            Console.WriteLine("el valor de 'e' es : " + suma);
+        }
+        static double Factorial(double valor)
+        {
+            if (valor <= 1) return 1;
+            else
+            {
+                return valor * Factorial(valor - 1);
+            }
+        }
+    }
+}
---
+++ Seno.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Coseno
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Ingrese el valor de X");
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor de iteraciones");
+            int n = int.Parse(Console.ReadLine());
+            double suma = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                suma += (Math.Pow(-1, i) / Factorial(2 * i )) * (Math.Pow(x, (2 * i)));
+            }
+            Console.WriteLine("El valor del Coseno es  : " + suma);
+        }
+
+        static double Factorial(double valor)
+        {
+            if (valor <= 1) return 1;
+            return valor * Factorial(valor - 1);
+        }
+    }
+}
