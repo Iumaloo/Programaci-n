70eb9941b62c67253c3d9b00ba4ff9b530f78023
Iumaloo <67984789+Iumaloo@users.noreply.github.com>
2021-10-17 20:37:46 -0500

    Add files via upload

---
+++ Dog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObserverDogCsharp
+{
+    public class Dog : Observer
+    {
+        public Cat cat;
+        public Dog (string name): base( name)
+        {
+
+        }
+
+       public  override void Notify(Observer observer,Subject subject)
+        {
+           
+        }
+       public void Smell(Dog dog,Cat cat )
+        {
+            cat.RegisterObserver(cat, this);
+            Console.WriteLine(this.name + " Just smelled " + cat.name);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ObserverDogCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat hopkins = new Cat("Hopkins", 1);
+            Dog alfred = new Dog("Alfred");
+            alfred.Smell(alfred, hopkins);
+            hopkins.Speak(hopkins);
+        }
+    }
+}
